b2c6879abb0fcc27bff7fc65e5be916a80bdbb12
franciscatan-armedu <francisca.tan@arm.com>
2021-12-07 10:12:32 +0000

    Initial commit
    Initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+
+# Real-Time-Operating-Systems-Design-And-Programming-Education-Kit
+
+Welcome to our Real-Time Operating Systems Design And Programming Education Kit!
+
+### [Download the EdKit here](https://github.com/arm-university/Real-Time-Operating-Systems-Design-And-Programming-Education-Kit/archive/refs/heads/main.zip)
+
+Our flagship offering to universities worldwide is the Arm University Program Education Kit series.
+
+These self-contained educational materials offered exclusively and at no cost to academics and teaching staff worldwide. They’re designed to support your day-to-day teaching on core electronic engineering and computer science subjects. You have the freedom to choose which modules to teach – you can use all the modules in the Education Kit or only those that are most appropriate to your teaching outcomes.
+
+Our Real-Time Operating Systems Design And Programming Education Kit teaches your students how operating systems control individual devices, and how to enable the efficient functioning of device networks, especially in real-time environments. Given the complex tasks facing today’s computing devices, operating systems must be stable, fast and efficient, particularly given the interaction between devices that recent technologies such as the Internet of Things will bring. A full description of the education kit can be found [here](https://www.arm.com/resources/education/education-kits/real-time-operating-systems).
+
+
+ ## Kit specification:
+
+* A full set of lecture slides, ready for use in a typical 10-12-week undergraduate course (full syllabus below).
+* Lab manual with solutions for faculty. Labs use low cost, powerful hardware boards (donated by partners and subject to availability).
+* License for royalty-free Keil RTX Real-Time OS bundled with Keil MDK development software.
+* **Prerequisites:** Basics of programming
+
+## Course Aim
+To produce students who can design and program real-time operating systems on Arm-based platforms and use them to improve their application performance.
+
+## Syllabus
+1.	Introduction
+2.	OS Overview
+3.	Process, Task and Thread
+4.	Scheduling
+5.	Concurrency
+6.	Memory
+7.	Virtual Memory
+8.	File System and I/O
+9.	RTOS and RTX
+10.	RTX Task and Simple Time Management
+11.	Sharing Data on RTX
+12.	Performance Evaluation and OS-aware Debugging
+
+
+## License
+You are free to fork or clone this material. See [LICENSE.md](https://github.com/arm-university/Real-Time-Operating-Systems-Design-And-Programming-Education-Kit/blob/main/License/LICENSE.md) for the complete license.
+
+## Inclusive Language Commitment
+Arm is committed to making the language we use inclusive, meaningful, and respectful. Our goal is to remove and replace non-inclusive language from our vocabulary to reflect our values and represent our global ecosystem.
+ 
+Arm is working actively with our partners, standards bodies, and the wider ecosystem to adopt a consistent approach to the use of inclusive language and to eradicate and replace offensive terms. We recognise that this will take time. This course may contain references to non-inclusive language; it will be updated with newer terms as those terms are agreed and ratified with the wider community. We recognise that some of you will be accustomed to using the previous terms and may not immediately recognise their replacements. Please refer to the following examples:
+
+* When introducing the AMBA AXI Protocols, we will use the term ‘Manager’ instead of ‘Master’ and ‘Subordinate’ instead of ‘Slave’. 
+* When introducing the architecture, we will use the term ‘Requester’ instead of ‘Master’ and ‘Completer’ instead of ‘Slave’. 
+
+ 
+Contact us at education@arm.com with questions or comments about this course. You can also report non-inclusive and offensive terminology usage in Arm content at terms@arm.com.
---
+++ ST-DiscoveryF4/html/js/config.js
@@ -0,0 +1,220 @@
+var index = 
+{
+  "titles": {
+    "title": "Arm Education Kit",
+    "libname": "",
+    "libtype": "Real Time Operating Systems Design and Programming"
+  },
+  "headers": [
+    {
+      "name": "Lecture Presentation",
+      "column": "modules"
+    },
+    {
+      "name": "Lab Materials",
+      "column": "exercises"
+    }
+  ],
+  "modules": [
+    [
+    {
+        "column": "modules",
+        "name": "Module 1: Introduction to the Course",
+        "link": "contents/Module_1/Lecture Presentation.pptx"
+      },
+		{
+        "column": "exercises",
+        "name": "Getting Started",
+        "link": "contents/Appendix/Getting Started.docx"
+      }
+    ],
+    [
+      {
+        "column": "modules",
+        "name": "Module 2: OS Overview",
+        "link": "contents/Module_2/Lecture Presentation.pptx"
+      }
+    ],
+    [
+      {
+        "column": "modules",
+        "name": "Module 3: Process, Task and Thread",
+        "link": "contents/Module_3/Lecture Presentation.pptx"
+      },
+      {
+        "column": "exercises",
+        "name": "Lab Manual 1",
+        "link": "contents/Module_3/Lab Manual_1.docx"
+      },
+      {
+        "column": "exercises",
+        "name": "Lab Manual 2",
+        "link": "contents/Module_3/Lab Manual_2.docx"
+      },
+    {
+        "column": "exercises",
+        "name": "Code 1",
+        "link": "contents/Module_3/Code_1.zip"
+      },
+		{
+        "column": "exercises",
+        "name": "Code 2",
+        "link": "contents/Module_3/Code_1.zip"
+      },
+		{
+        "column": "exercises",
+        "name": "Code 2 Solution",
+        "link": "contents/Module_3/Code_2_Solution.zip"
+      },
+		
+    ],
+    [
+      {
+        "column": "modules",
+        "name": "Module 4: Scheduling",
+        "link": "contents/Module_4/Lecture Presentation.pptx"
+      },
+      {
+        "column": "exercises",
+        "name": "Homework",
+        "link": "contents/Module_4/Homework.docx"
+      },
+      {
+        "column": "exercises",
+        "name": "Homework Solutions",
+        "link": "contents/Module_4/Solutions.docx"
+      },
+        ],
+    [
+    {
+        "column": "modules",
+        "name": "Module 5: Concurrency",
+        "link": "contents/Module_5/Lecture Presentation.pptx"
+      },
+    {
+        "column": "exercises",
+        "name": "Lab Manual 3",
+        "link": "contents/Module_5/Lab Manual.docx"
+      },
+    {
+        "column": "exercises",
+        "name": "Code 3",
+        "link": "contents/Module_5/Code.zip"
+      },
+    {
+        "column": "exercises",
+        "name": "Code Solution 3",
+        "link": "contents/Module_5/Code_Solution.zip"
+        },
+    {
+        "column": "exercises",
+        "name": "Banker's Algorithm",
+        "link": "contents/Module_5/Banker's Algorithm.xlsx"
+      },
+    ],
+    [
+	  {
+        "column": "modules",
+        "name": "Module 6: Memory",
+        "link": "contents/Module_6/Lecture Presentation.pptx"
+      }
+  
+    ],
+    [
+      {
+        "column": "modules",
+        "name": "Module 7: Virtual Memory",
+        "link": "contents/Module_7/Lecture Presentation.pptx"
+      },
+    {
+        "column": "exercises",
+        "name": "Homework",
+        "link": "contents/Module_7/Homework.docx"
+      },
+    {
+        "column": "exercises",
+        "name": "Virtual Memory",
+        "link": "contents/Module_7/Virtual Memory.xlsx"
+      }
+    
+    ],
+    [
+      {
+        "column": "modules",
+        "name": "Module 8: File System and I/O",
+        "link": "contents/Module_8/Lecture Presentation.pptx"
+      }
+    
+    ],
+    [
+      {
+        "column": "modules",
+        "name": "Module 9: RTOS and RTX",
+        "link": "contents/Module_9/Lecture Presentation.pptx"
+      }
+    
+    ],
+    [
+	  {
+        "column": "modules",
+        "name": "Module 10: RTX Task and Simple Time Management",
+        "link": "contents/Module_10/Lecture Presentation.pptx"
+      },
+    {
+        "column": "exercises",
+        "name": "Lab Manual 4",
+        "link": "contents/Module_10/Lab Manual.docx"
+      },
+    {
+        "column": "exercises",
+        "name": "Code 4",
+        "link": "contents/Module_10/Code.zip"
+      }
+    
+    ],
+    [
+      {
+        "column": "modules",
+        "name": "Module 11: Sharing Data on RTX",
+        "link": "contents/Module_11/Lecture Presentation.pptx"
+      },
+    {
+        "column": "exercises",
+        "name": "Lab Manual 5",
+        "link": "contents/Module_11/Lab Manual.docx"
+      },
+    {
+        "column": "exercises",
+        "name": "Code 5",
+        "link": "contents/Module_11/Code.zip"
+      },
+    {
+        "column": "exercises",
+        "name": "Code 5 Solution",
+        "link": "contents/Module_11/Code_Solution.zip"
+      }
+    ],
+    [
+		{
+        "column": "modules",
+        "name": "Module 12: Performance Evaluation and OS-aware Debugging",
+        "link": "contents/Module_12/Lecture Presentation.pptx"
+      },
+     {
+        "column": "exercises",
+        "name": "Lab Manual 6",
+        "link": "contents/Module_12/Lab Manual.docx"
+      },
+    {
+        "column": "exercises",
+        "name": "Code 6",
+        "link": "contents/Module_12/Code.zip"
+      },
+    {
+        "column": "exercises",
+        "name": "Code 6 Solution",
+        "link": "contents/Module_12/Code_Solution.zip"
+      }
+    ]
+  ]
+}
