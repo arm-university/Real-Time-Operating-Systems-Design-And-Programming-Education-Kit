cd227094a5fa3468e5694123fe3f2a64daee8f3b
Richard Cook <r.a.cook@soton.ac.uk>
2022-12-08 17:30:38 +0000

    Fix paths in config.js

--- ST-DiscoveryF4/html/js/config.js
+++ ST-DiscoveryF4/html/js/config.js
@@ -20,7 +20,7 @@ var index =
     {
         "column": "modules",
         "name": "Module 1: Introduction to the Course",
-        "link": "contents/Module_1/Lecture Presentation.pptx"
+        "link": "contents/Module 01_Introduction/Lecture Presentation.pptx"
       },
 		{
         "column": "exercises",
@@ -32,39 +32,39 @@ var index =
       {
         "column": "modules",
         "name": "Module 2: OS Overview",
-        "link": "contents/Module_2/Lecture Presentation.pptx"
+        "link": "contents/Module 02_OS Overview/Lecture Presentation.pptx"
       }
     ],
     [
       {
         "column": "modules",
         "name": "Module 3: Process, Task and Thread",
-        "link": "contents/Module_3/Lecture Presentation.pptx"
+        "link": "contents/Module 03_Process, Task and Thread/Lecture Presentation.pptx"
       },
       {
         "column": "exercises",
         "name": "Lab Manual 1",
-        "link": "contents/Module_3/Lab Manual_1.docx"
+        "link": "contents/Module 03_Process, Task and Thread/Lab Manual_1.docx"
       },
       {
         "column": "exercises",
         "name": "Lab Manual 2",
-        "link": "contents/Module_3/Lab Manual_2.docx"
+        "link": "contents/Module 03_Process, Task and Thread/Lab Manual_2.docx"
       },
     {
         "column": "exercises",
         "name": "Code 1",
-        "link": "contents/Module_3/Code_1.zip"
+        "link": "contents/Module 03_Process, Task and Thread/Code_1"
       },
 		{
         "column": "exercises",
         "name": "Code 2",
-        "link": "contents/Module_3/Code_1.zip"
+        "link": "contents/Module 03_Process, Task and Thread/Code_2"
       },
 		{
         "column": "exercises",
         "name": "Code 2 Solution",
-        "link": "contents/Module_3/Code_2_Solution.zip"
+        "link": "contents/Module 03_Process, Task and Thread/Code_2_Solution"
       },
 		
     ],
@@ -72,51 +72,51 @@ var index =
       {
         "column": "modules",
         "name": "Module 4: Scheduling",
-        "link": "contents/Module_4/Lecture Presentation.pptx"
+        "link": "contents/Module 04_Scheduling/Lecture Presentation.pptx"
       },
       {
         "column": "exercises",
         "name": "Homework",
-        "link": "contents/Module_4/Homework.docx"
+        "link": "contents/Module 04_Scheduling/Homework.docx"
       },
       {
         "column": "exercises",
         "name": "Homework Solutions",
-        "link": "contents/Module_4/Solutions.docx"
+        "link": "contents/Module 04_Scheduling/Solutions.docx"
       },
         ],
     [
     {
         "column": "modules",
         "name": "Module 5: Concurrency",
-        "link": "contents/Module_5/Lecture Presentation.pptx"
+        "link": "contents/Module 05_Concurrency/Lecture Presentation.pptx"
       },
     {
         "column": "exercises",
         "name": "Lab Manual 3",
-        "link": "contents/Module_5/Lab Manual.docx"
+        "link": "contents/Module 05_Concurrency/Lab Manual.docx"
       },
     {
         "column": "exercises",
         "name": "Code 3",
-        "link": "contents/Module_5/Code.zip"
+        "link": "contents/Module 05_Concurrency/Code.zip"
       },
     {
         "column": "exercises",
         "name": "Code Solution 3",
-        "link": "contents/Module_5/Code_Solution.zip"
+        "link": "contents/Module 05_Concurrency/Code_Solution"
         },
     {
         "column": "exercises",
         "name": "Banker's Algorithm",
-        "link": "contents/Module_5/Banker's Algorithm.xlsx"
+        "link": "contents/Module 05_Concurrency/Banker's Algorithm.xlsx"
       },
     ],
     [
 	  {
         "column": "modules",
         "name": "Module 6: Memory",
-        "link": "contents/Module_6/Lecture Presentation.pptx"
+        "link": "contents/Module 06_Memory/Lecture Presentation.pptx"
       }
   
     ],
@@ -124,17 +124,17 @@ var index =
       {
         "column": "modules",
         "name": "Module 7: Virtual Memory",
-        "link": "contents/Module_7/Lecture Presentation.pptx"
+        "link": "contents/Module 07_Virtual Memory/Lecture Presentation.pptx"
       },
     {
         "column": "exercises",
         "name": "Homework",
-        "link": "contents/Module_7/Homework.docx"
+        "link": "contents/Module 07_Virtual Memory/Homework.docx"
       },
     {
         "column": "exercises",
         "name": "Virtual Memory",
-        "link": "contents/Module_7/Virtual Memory.xlsx"
+        "link": "contents/Module 07_Virtual Memory/Virtual Memory.xlsx"
       }
     
     ],
@@ -142,7 +142,7 @@ var index =
       {
         "column": "modules",
         "name": "Module 8: File System and I/O",
-        "link": "contents/Module_8/Lecture Presentation.pptx"
+        "link": "contents/Module 08_File System and IO/Lecture Presentation.pptx"
       }
     
     ],
@@ -150,7 +150,7 @@ var index =
       {
         "column": "modules",
         "name": "Module 9: RTOS and RTX",
-        "link": "contents/Module_9/Lecture Presentation.pptx"
+        "link": "contents/Module 09_RTOS and RTX/Lecture Presentation.pptx"
       }
     
     ],
@@ -158,17 +158,17 @@ var index =
 	  {
         "column": "modules",
         "name": "Module 10: RTX Task and Simple Time Management",
-        "link": "contents/Module_10/Lecture Presentation.pptx"
+        "link": "contents/Module 10_RTX Task and Simple Time Management/Lecture Presentation.pptx"
       },
     {
         "column": "exercises",
         "name": "Lab Manual 4",
-        "link": "contents/Module_10/Lab Manual.docx"
+        "link": "contents/Module 10_RTX Task and Simple Time Management/Lab Manual.docx"
       },
     {
         "column": "exercises",
         "name": "Code 4",
-        "link": "contents/Module_10/Code.zip"
+        "link": "contents/Module 10_RTX Task and Simple Time Management/Code"
       }
     
     ],
@@ -176,44 +176,44 @@ var index =
       {
         "column": "modules",
         "name": "Module 11: Sharing Data on RTX",
-        "link": "contents/Module_11/Lecture Presentation.pptx"
+        "link": "contents/Module 11_Sharing Data on RTX/Lecture Presentation.pptx"
       },
     {
         "column": "exercises",
         "name": "Lab Manual 5",
-        "link": "contents/Module_11/Lab Manual.docx"
+        "link": "contents/Module 11_Sharing Data on RTX/Lab Manual.docx"
       },
     {
         "column": "exercises",
         "name": "Code 5",
-        "link": "contents/Module_11/Code.zip"
+        "link": "contents/Module 11_Sharing Data on RTX/Code"
       },
     {
         "column": "exercises",
         "name": "Code 5 Solution",
-        "link": "contents/Module_11/Code_Solution.zip"
+        "link": "contents/Module 11_Sharing Data on RTX/Code_Solution"
       }
     ],
     [
 		{
         "column": "modules",
         "name": "Module 12: Performance Evaluation and OS-aware Debugging",
-        "link": "contents/Module_12/Lecture Presentation.pptx"
+        "link": "contents/Module 12_Performance Evaluation and OS-aware Debugging/Lecture Presentation.pptx"
       },
      {
         "column": "exercises",
         "name": "Lab Manual 6",
-        "link": "contents/Module_12/Lab Manual.docx"
+        "link": "contents/Module 12_Performance Evaluation and OS-aware Debugging/Lab Manual.docx"
       },
     {
         "column": "exercises",
         "name": "Code 6",
-        "link": "contents/Module_12/Code.zip"
+        "link": "contents/Module 12_Performance Evaluation and OS-aware Debugging/Code"
       },
     {
         "column": "exercises",
         "name": "Code 6 Solution",
-        "link": "contents/Module_12/Code_Solution.zip"
+        "link": "contents/Module 12_Performance Evaluation and OS-aware Debugging/Code_Solution"
       }
     ]
   ]
